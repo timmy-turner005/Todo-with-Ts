df67018d0b86a9095fe1d3037bb48aa235bfae03
timmy-turner005 <timitalented005@gmail.com>
2024-07-02 16:08:43 +0100

    first commit

---
+++ src/models/todo.ts
@@ -0,0 +1,13 @@
+// you could use the "class" keyword or the "type" keyword or the "interface" keyword.
+
+class Todo {
+  id: string;
+  text: string;
+
+  constructor(todoText: string) {
+    this.text = todoText;
+    this.id = new Date().toISOString();
+  }
+}
+
+export default Todo;
